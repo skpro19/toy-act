8e1cf120ce9e2537fd8eaef0e813ce66cdf79280
skpro19 <shubhamkumarpro1@gmail.com>
2026-09-14 16:41:38 +0530

    chore: extend shared config and vscode spellright settings

--- scripts/config.py
+++ scripts/config.py
@@ -1,7 +1,11 @@
 """ Shared architectural constants """
 
 ## Transformer (encoder / decoder)
-D_MODEL = 512
+D_MODEL = 512 # input token feat dimensiosn
+NUM_IMG_TOKENS = 128
+NUM_PROPRIO_TOKENS = 1
+NUM_LAYERS = 4
+
 
 ## Transformer Encoder
 N_HEAD = 4
