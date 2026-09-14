1169238ec24f499681a012f6f245cfa6e0d6604e
skpro19 <shubhamkumarpro1@gmail.com>
2026-09-15 01:38:24 +0530

    feat: add image encoder with conv stem and token projection

---
+++ scripts/image_encoder.py
@@ -0,0 +1,46 @@
+"""
+- image to tokens
+- assume single camera setup 
+"""
+
+import torch
+from torch import nn
+from scripts.config import D_MODEL
+
+class ImageEncoder(nn.Module): 
+
+    def __init__(self, d_model:int = D_MODEL): 
+        super().__init__()
+
+        self.d_model = d_model
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1)
+        
+        self.relu  = nn.ReLU()
+        self.project = nn.Linear(in_features=128, out_features=self.d_model)
+
+    def forward(self, x: torch.Tensor):
+        
+        x = self.conv1(x)
+        x = self.relu(x)
+
+        # print(f"x.shape => {x.shape}")
+
+        x = self.conv2(x)
+        x = self.relu(x)
+        
+        # print(f"x.shape => {x.shape}")
+
+        x = self.conv3(x)
+        x = self.relu(x)
+
+        # print(f"x.shape => {x.shape}")
+
+        B,C,H,W = x.shape
+        x = x.permute(0,2,3,1).reshape(B, H*W, C)
+
+        # 128 -> d_model
+        x = self.project(x)
+
+        return x
